c4a9132910f6a3a815570b389839201e8b8a74ee
elijaholmos <eli.olmos9@gmail.com>
2023-08-30 15:20:08 -0700

    fix: remove duplicate articles from response

--- scrapers/bloomberg.js
+++ scrapers/bloomberg.js
@@ -10,43 +10,41 @@ export async function run({ page, ticker }) {
 	await page.goto(`https://www.bnnbloomberg.ca/search/bnn-search-7.337157?q=.`);
 
 	await page.waitForSelector('.search-results');
-	const news = await page.$eval(
-		'.search-results',
-		(parentEl, KEYWORDS) => {
-			// recursively search each element for keywords
-			const deepSearch = (el, matches = []) => {
-				for (const word of KEYWORDS)
-					if (
-						el?.getAttribute('ng-bind-html')?.contains('result.Name') &&
-						el?.innerText?.toLowerCase()?.includes(word.toLowerCase())
-					)
-						matches.push({
-							title: el.innerText,
-							url: el.closest('a')?.href,
-						});
-				// if (!el?.children?.length) {
-				// 	for (const word of KEYWORDS)
-				// 		if (el?.innerText?.toLowerCase()?.includes(word.toLowerCase())) matches.push(el);
-				// 	return matches;
-				// }
-				if (!el?.children?.length) return matches;
-				for (const child of el.children) deepSearch(child, matches);
-				return matches;
-			};
+	const news = (
+		await page.$eval(
+			'.search-results',
+			(parentEl, KEYWORDS) => {
+				// recursively search each element for keywords
+				const deepSearch = (el, matches = []) => {
+					for (const word of KEYWORDS)
+						if (
+							el?.getAttribute('ng-bind-html')?.contains('result.Name') &&
+							el?.innerText?.toLowerCase()?.includes(word.toLowerCase())
+						)
+							matches.push({
+								title: el.innerText,
+								url: el.closest('a')?.href,
+							});
+					// if (!el?.children?.length) {
+					// 	for (const word of KEYWORDS)
+					// 		if (el?.innerText?.toLowerCase()?.includes(word.toLowerCase())) matches.push(el);
+					// 	return matches;
+					// }
+					if (!el?.children?.length) return matches;
+					for (const child of el.children) deepSearch(child, matches);
+					return matches;
+				};
 
-			return deepSearch(parentEl);
-		},
-		KEYWORDS
-	);
+				return deepSearch(parentEl);
+			},
+			KEYWORDS
+		)
+	).reduce((arr, item) => {
+		// remove dups
+		if (!arr.some(({ url }) => item.url === url)) arr.push(item);
+		return arr;
+	}, []);
 	console.log('news', news);
-
-	// for (const el of res) {
-	// 	if (el?.getAttribute('ng-bind-html')?.contains('result.Name'))
-	// 		news.push({
-	// 			title: el.innerText,
-	// 			url: el.closest('a')?.href,
-	// 		});
-	// }
 
 	return news;
 }
